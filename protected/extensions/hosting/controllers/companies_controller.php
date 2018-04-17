c0fcd2ab9b6ea3066ee2dce60ca8c9166a1baec3
Farid Efendi <miftahfaridse@gmail.com>
2018-04-17 14:30:55 +0700

    Feature and company creation

--- protected/extensions/hosting/controllers/companies_controller.php
+++ protected/extensions/hosting/controllers/companies_controller.php
@@ -17,13 +17,18 @@ class CompaniesController extends BaseController
         $app->map(['GET', 'POST'], '/create', [$this, 'create']);
         $app->map(['GET', 'POST'], '/update/[{id}]', [$this, 'update']);
         $app->map(['POST'], '/delete/[{id}]', [$this, 'delete']);
+        $app->map(['POST'], '/create-plan/[{id}]', [$this, 'create_plan']);
+        $app->map(['POST'], '/delete-plan/[{id}]', [$this, 'delete_plan']);
     }
 
     public function accessRules()
     {
         return [
             ['allow',
-                'actions' => ['view', 'create', 'update', 'delete'],
+                'actions' => [
+                    'view', 'create', 'update', 'delete',
+                    'create-plan', 'delete-plan'
+                    ],
                 'users'=> ['@'],
             ],
             ['deny',
@@ -60,75 +65,30 @@ class CompaniesController extends BaseController
             return $this->notAllowedAction();
         }
 
-        $languages = \ExtensionsModel\PostLanguageModel::model()->findAll();
-        $model = new \ExtensionsModel\PostModel('create');
-        $categories = \ExtensionsModel\PostCategoryModel::model()->findAll();
-        $post_id = 0;
-
-        if (isset($_POST['Post'])){
-            $model->status = $_POST['Post']['status'];
-            $model->allow_comment = ($_POST['Post']['allow_comment'] == 'on')? 1 : 0;
-            $model->post_type = $_POST['Post']['post_type'];
-            $model->author_id = $this->_user->id;
-            if (!empty($_POST['Post']['tags'])) {
-                $model->tags = $_POST['Post']['tags'];
-            }
+        if (isset($_POST['HostingCompany'])){
+            $model = new \ExtensionsModel\HostingCompanyModel('create');
+            $model->title = $_POST['HostingCompany']['title'];
+            $model->description = $_POST['HostingCompany']['description'];
+            $model->address = $_POST['HostingCompany']['address'];
+            $model->phone = $_POST['HostingCompany']['phone'];
+            $model->email = $_POST['HostingCompany']['email'];
+            $model->website = $_POST['HostingCompany']['website'];
+            $model->status = $_POST['HostingCompany']['status'];
             $model->created_at = date('Y-m-d H:i:s');
-            $model->updated_at = date('Y-m-d H:i:s');
-            $create = \ExtensionsModel\PostModel::model()->save(@$model);
-            if ($create > 0) {
-                $post_content = \ExtensionsModel\PostContentModel::model();
-                foreach ($_POST['PostContent']['title'] as $lang => $title) {
-                    if (!empty($title) && !empty($_POST['PostContent']['content'][$lang])) {
-                        $model2 = new \ExtensionsModel\PostContentModel;
-                        $model2->post_id = $model->id;
-                        $model2->title = $title;
-                        if (!empty($_POST['PostContent']['slug'][$lang])){
-                            $cek_slug = $post_content->findByAttributes(['slug'=>$_POST['PostContent']['slug'][$lang]]);
-                            if ($cek_slug instanceof \RedBeanPHP\OODBBean) {
-                                $model2->slug = $_POST['PostContent']['slug'][$lang].'2';
-                            } else {
-                                $model2->slug = $_POST['PostContent']['slug'][$lang];
-                            }
-                        } else
-                            $model2->slug = $model->createSlug($title);
-
-                        $model2->language = $lang;
-                        $model2->content = $_POST['PostContent']['content'][$lang];
-                        $model2->meta_keywords = $_POST['PostContent']['meta_keywords'][$lang];
-                        $model2->meta_description = $_POST['PostContent']['meta_description'][$lang];
-                        $model2->created_at = date("Y-m-d H:i:s");
-                        $model2->updated_at = date("Y-m-d H:i:s");
-                        $create_content = $post_content->save($model2);
-                    }
-                }
-                $post_in_category = \ExtensionsModel\PostInCategoryModel::model();
-                if (!empty($_POST['Post']['post_category']) && is_array($_POST['Post']['post_category'])) {
-                    foreach ($_POST['Post']['post_category'] as $ci => $category_id) {
-                        $model3 = new \ExtensionsModel\PostInCategoryModel();
-                        $model3->post_id = $model->id;
-                        $model3->category_id = $category_id;
-                        $model3->created_at = date("Y-m-d H:i:s");
-                        $post_in_category->save($model3);
-                    }
-                }
-
-                $message = 'Your post is successfully created.';
+            $model->created_by = $this->_user->id;
+            $save = \ExtensionsModel\HostingCompanyModel::model()->save($model);
+            if ($save) {
+                $message = 'Data berhasil disimpan';
                 $success = true;
-                $post_id = $model->id;
             } else {
-                $message = 'Failed to create new post.';
+                $message = 'Gagal menyimpan data.';
                 $success = false;
             }
         }
 
         return $this->_container->module->render($response, 'hostings/create.html', [
-            'languages' => $languages,
-            'status_list' => $model->getListStatus(),
-            'categories' => $categories,
             'message' => ($message) ? $message : null,
-            'success' => $success,
-            'post_id' => $post_id
+            'success' => $success
         ]);
     }
 
@@ -148,6 +108,9 @@ class CompaniesController extends BaseController
         $model = \ExtensionsModel\HostingCompanyModel::model()->findByPk($args['id']);
         $hcmodel = new \ExtensionsModel\HostingCompanyModel();
         $detail = $hcmodel->getDetail($args['id']);
+        // hosting plan
+        $hpmodel = new \ExtensionsModel\HostingPlanModel();
+        $plans = $hpmodel->getData(['hosting_company_id' => $model->id]);
 
         if (isset($_POST['HostingCompany'])){
             $model->title = $_POST['HostingCompany']['title'];
@@ -155,6 +118,7 @@ class CompaniesController extends BaseController
             $model->address = $_POST['HostingCompany']['address'];
             $model->phone = $_POST['HostingCompany']['phone'];
             $model->email = $_POST['HostingCompany']['email'];
+            $model->website = $_POST['HostingCompany']['website'];
             $model->status = $_POST['HostingCompany']['status'];
             $model->updated_at = date('Y-m-d H:i:s');
             $model->updated_by = $this->_user->id;
@@ -173,7 +137,8 @@ class CompaniesController extends BaseController
             'model' => $model,
             'detail' => $detail,
             'message' => ($message) ? $message : null,
-            'success' => $success
+            'success' => $success,
+            'plans' => $plans
         ]);
     }
 
@@ -191,13 +156,84 @@ class CompaniesController extends BaseController
             return false;
         }
 
-        $model = \ExtensionsModel\PostModel::model()->findByPk($args['id']);
-        $delete = \ExtensionsModel\PostModel::model()->delete($model);
+        $model = \ExtensionsModel\HostingCompanyModel::model()->findByPk($args['id']);
+        $delete = \ExtensionsModel\HostingCompanyModel::model()->delete($model);
         if ($delete) {
-            $delete2 = \ExtensionsModel\PostContentModel::model()->deleteAllByAttributes(['post_id'=>$args['id']]);
-            $delete3 = \ExtensionsModel\PostInCategoryModel::model()->deleteAllByAttributes(['post_id'=>$args['id']]);
-            $message = 'Your page is successfully created.';
+            $delete2 = \ExtensionsModel\HostingPlanModel::model()->deleteAllByAttributes(['hosting_company_id'=>$args['id']]);
+            $message = 'Data berhasil dihapus.';
             echo true;
+        }
+    }
+
+    public function create_plan($request, $response, $args)
+    {
+        $isAllowed = $this->isAllowed($request, $response);
+        if ($isAllowed instanceof \Slim\Http\Response)
+            return $isAllowed;
+
+        if (!$isAllowed) {
+            return $this->notAllowedAction();
+        }
+
+        if (!isset($args['id'])) {
+            return false;
+        }
+
+        $model = new \ExtensionsModel\HostingPlanModel();
+        if (isset($_POST['HostingPlan'])) {
+            $model->title = $_POST['HostingPlan']['title'];
+            $model->description = $_POST['HostingPlan']['description'];
+            $model->hosting_company_id = $args['id'];
+            $model->created_at = date("Y-m-d H:i:s");
+            $model->created_by = $this->_user->id;
+            try {
+                $save = \ExtensionsModel\HostingPlanModel::model()->save($model);
+            } catch (\Exception $e) {
+                var_dump($e->getMessage()); exit;
+            }
+
+            if ($save) {
+                return $response->withJson(
+                    [
+                        'status' => 'success',
+                        'message' => 'Data berhasil disimpan.',
+                    ], 201);
+            } else {
+                return $response->withJson(['status'=>'failed'], 201);
+            }
+        } else {
+            return $response->withJson(['status'=>'failed', 'messsage'=>'Tidak berhasil menyimpan data.'], 201);
+        }
+    }
+
+    public function delete_plan($request, $response, $args)
+    {
+        $isAllowed = $this->isAllowed($request, $response, $args);
+        if ($isAllowed instanceof \Slim\Http\Response)
+            return $isAllowed;
+
+        if(!$isAllowed){
+            return $this->notAllowedAction();
+        }
+
+        if (!isset($args['id'])) {
+            return false;
+        }
+
+        $model = \ExtensionsModel\HostingPlanModel::model()->findByPk($args['id']);
+        $delete = \ExtensionsModel\HostingPlanModel::model()->delete($model);
+        if ($delete) {
+            return $response->withJson(
+                [
+                    'status' => 'success',
+                    'message' => 'Data berhasil dihapus.',
+                ], 201);
+        } else {
+            return $response->withJson(
+                [
+                    'status' => 'failed',
+                    'message' => 'Data gagal dihapus.',
+                ], 201);
         }
     }
 }
---
+++ protected/extensions/hosting/models/hosting_plan.php
@@ -0,0 +1,75 @@
+<?php
+namespace ExtensionsModel;
+
+require_once __DIR__ . '/../../../models/base.php';
+
+class HostingPlanModel extends \Model\BaseModel
+{
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public function tableName()
+    {
+        return 'ext_hosting_plan';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return [
+            ['title', 'required'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getData($data = null)
+    {
+        $sql = 'SELECT t.*, a.name AS admin_name  
+            FROM {tablePrefix}ext_hosting_plan t 
+            LEFT JOIN {tablePrefix}admin a ON a.id = t.created_by 
+            WHERE 1';
+
+        $params = [];
+        if (is_array($data)) {
+            if (isset($data['hosting_company_id'])) {
+                $sql .= ' AND t.hosting_company_id =:hosting_company_id';
+                $params['hosting_company_id'] = $data['hosting_company_id'];
+            }
+        }
+
+        $sql .= ' ORDER BY t.created_at DESC';
+
+        $sql = str_replace(['{tablePrefix}'], [$this->_tbl_prefix], $sql);
+
+        $rows = \Model\R::getAll( $sql, $params );
+
+        return $rows;
+    }
+
+    /**
+     * @param $id
+     * @return array
+     */
+    public function getDetail($id)
+    {
+        $sql = 'SELECT t.*,  a.name AS created_by_name, ab.name AS updated_by_name 
+            FROM {tablePrefix}ext_hosting_plan t 
+            LEFT JOIN {tablePrefix}admin a ON a.id = t.created_by 
+            LEFT JOIN {tablePrefix}admin ab ON ab.id = t.updated_by 
+            WHERE t.id =:id';
+
+        $sql = str_replace(['{tablePrefix}'], [$this->_tbl_prefix], $sql);
+
+        $row = \Model\R::getRow( $sql, ['id'=>$id] );
+
+        return $row;
+    }
+}
